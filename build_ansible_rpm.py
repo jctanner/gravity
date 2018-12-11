a50a77b930db41e4bf34a38d987dbb9355b21d33
James Tanner <tanner.jc@gmail.com>
2018-12-11 11:41:58 -0500

    kill fragments in the rpm

--- build_ansible_rpm.py
+++ build_ansible_rpm.py
@@ -10,6 +10,15 @@ from sh import find
 from sh import git
 from sh import make
 
+from logzero import logger
+
+
+FRAGMENT_WHITELIST = [
+    '__init__.py',
+    'default_callback.py',
+    'shell_common.py',
+]
+
 
 MODULE_WHITELIST = [
     'modules/__init__.py',
@@ -42,63 +51,88 @@ def main():
     src_dir = '/tmp/ansible.mazer.checkout'
     dst_dir = '/tmp/ansible.mazer.build'
 
-    if not args.noclean:
+    if not os.path.exists(src_dir):
+        logger.info('cloning source repo')
+        git.clone('--branch=%s' % src_branch, src_repo, src_dir)
 
-        if not os.path.exists(src_dir):
-            git.clone('--branch=%s' % src_branch, src_repo, src_dir)
+    logger.info('copying source checkout to build checkout')
+    if os.path.exists(dst_dir):
+        shutil.rmtree(dst_dir)
+    shutil.copytree(src_dir, dst_dir)
 
-        if os.path.exists(dst_dir):
-            shutil.rmtree(dst_dir)
-        shutil.copytree(src_dir, dst_dir)
+    logger.info('cleaning out fragments')
+    mdfd = os.path.join(dst_dir, 'lib', 'ansible', 'utils', 'module_docs_fragments')
+    fragments = find(mdfd)
+    fragments = [x.strip() for x in fragments if x.strip()]
+    for ff in fragments:
+        if not os.path.exists(ff):
+            continue
+        if ff == mdfd:
+            continue
+        found = False
+        for wl in FRAGMENT_WHITELIST:
+            if wl in ff:
+                found = True
+                break
+        if not found:
+            if os.path.isdir(ff):
+                shutil.rmtree(ff)
+            else:
+                os.remove(ff)
 
-        module_files = find(os.path.join(dst_dir, 'lib', 'ansible', 'modules'))
-        module_files = [x.strip() for x in module_files]
-        for mf in module_files:
-            if mf.endswith('/modules'):
-                continue
-            if not os.path.exists(mf):
-                continue
-            found = False
-            for wl in MODULE_WHITELIST:
-                if wl in mf:
-                    found = True
-                    break
-            if not found:
-                if os.path.isdir(mf):
-                    shutil.rmtree(mf)
-                else:
-                    os.remove(mf)
+    logger.info('cleaning out modules')
+    module_files = find(os.path.join(dst_dir, 'lib', 'ansible', 'modules'))
+    module_files = [x.strip() for x in module_files]
+    for mf in module_files:
+        if mf.endswith('/modules'):
+            continue
+        if not os.path.exists(mf):
+            continue
+        found = False
+        for wl in MODULE_WHITELIST:
+            if wl in mf:
+                found = True
+                break
+        if not found:
+            if os.path.isdir(mf):
+                shutil.rmtree(mf)
+            else:
+                os.remove(mf)
 
-        module_util_files = find(os.path.join(dst_dir, 'lib', 'ansible', 'module_utils'))
-        module_util_files = [x.strip() for x in module_util_files]
-        for muf in module_util_files:
-            if muf.endswith('/module_utils'):
-                continue
-            if not os.path.exists(muf):
-                continue
-            found = False
-            for wl in MODULE_UTIL_WHITELIST:
-                if wl in muf:
-                    found = True
-                    break
-            if not found:
-                if os.path.isdir(muf):
-                    shutil.rmtree(muf)
-                else:
-                    os.remove(muf)
+    logger.info('cleaning out module utils')
+    module_util_files = find(os.path.join(dst_dir, 'lib', 'ansible', 'module_utils'))
+    module_util_files = [x.strip() for x in module_util_files]
+    for muf in module_util_files:
+        if muf.endswith('/module_utils'):
+            continue
+        if not os.path.exists(muf):
+            continue
+        found = False
+        for wl in MODULE_UTIL_WHITELIST:
+            if wl in muf:
+                found = True
+                break
+        if not found:
+            if os.path.isdir(muf):
+                shutil.rmtree(muf)
+            else:
+                os.remove(muf)
 
-        try:
-            make.clean(_cwd=dst_dir)
-            make('rpm', _cwd=dst_dir)
-        except Exception as e:
-            print(e.stdout)
-            sys.exit(1)
+    logger.info('running "make rpm"')
+    try:
+        make.clean(_cwd=dst_dir)
+        make('rpm', _cwd=dst_dir)
+    except Exception as e:
+        print(e.stdout)
+        sys.exit(1)
 
+    logger.info('cleaning out old repo metadata')
     repodir = '/var/cache/gravity/repos/rpm'
     repodata_dir = os.path.join(repodir, 'repodata')
     if os.path.exists(repodata_dir):
         shutil.rmtree(repodata_dir)
 
+    logger.info('removing old ansible rpms from repo path')
     rpms = find(
         repodir,
         '-maxdepth',
@@ -108,6 +142,7 @@ def main():
     for rpm in rpms:
         os.remove(rpm.strip())
 
+    logger.info('copying ansible rpms to new repo path')
     rpms = find(
         os.path.join(dst_dir, 'rpm-build'),
         '-maxdepth',
@@ -117,6 +152,7 @@ def main():
     for rpm in rpms:
         shutil.copy(rpm.strip(), repodir)
 
+    logger.info('creating repo')
     createrepo('.', _cwd=repodir)
 
 
