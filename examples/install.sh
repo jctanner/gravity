0e9929d5bf1a8ec1e5eafe72f5031fea72c51406
James Tanner <tanner.jc@gmail.com>
2019-03-07 23:44:25 -0500

    checkin

--- examples/install.sh
+++ examples/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# curl https://raw.githubusercontent.com/jctanner/gravity/master/examples/install.sh | bash -
+
 rpm -q epel-release || yum -y install epel-release
 if [[ ! -f /etc/yum.repos.d/collections.repo ]]; then
     curl -o /etc/yum.repos.d/collections.repo http://tannerjc.net/ansible/collections.repo
@@ -8,3 +10,10 @@ fi
 yum clean all
 yum -y install ansible
 yum -y install ansible-collection-system
+
+FILENAMES="site_1.yml site_2.yml site_3.yml site_4.yml site_5.yml"
+for FN in $FILENAMES; do
+    if [[ ! -f $FN ]]; then
+        curl -o $FN https://raw.githubusercontent.com/jctanner/gravity/master/examples/$FN
+    fi
+done
